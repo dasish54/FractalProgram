4534f649f8a8c60e5c06e600c46f389ca76bfd2e
dasish54 <33153620+dasish54@users.noreply.github.com>
2018-03-13 12:49:57 +0000

    Added File via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AssignmentCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveStateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadStateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveImageToolStripMenuItem,
+            this.saveStateToolStripMenuItem,
+            this.loadStateToolStripMenuItem,
+            this.greyscaleToolStripMenuItem,
+            this.exitApplicationToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+            // 
+            // saveStateToolStripMenuItem
+            // 
+            this.saveStateToolStripMenuItem.Name = "saveStateToolStripMenuItem";
+            this.saveStateToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.saveStateToolStripMenuItem.Text = "Save State";
+            this.saveStateToolStripMenuItem.Click += new System.EventHandler(this.saveStateToolStripMenuItem_Click);
+            // 
+            // loadStateToolStripMenuItem
+            // 
+            this.loadStateToolStripMenuItem.Name = "loadStateToolStripMenuItem";
+            this.loadStateToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.loadStateToolStripMenuItem.Text = "Load State";
+            this.loadStateToolStripMenuItem.Click += new System.EventHandler(this.loadStateToolStripMenuItem_Click);
+            // 
+            // exitApplicationToolStripMenuItem
+            // 
+            this.exitApplicationToolStripMenuItem.Name = "exitApplicationToolStripMenuItem";
+            this.exitApplicationToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exitApplicationToolStripMenuItem.Text = "Exit Application";
+            this.exitApplicationToolStripMenuItem.Click += new System.EventHandler(this.exitApplicationToolStripMenuItem_Click);
+            // 
+            // greyscaleToolStripMenuItem
+            // 
+            this.greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            this.greyscaleToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.greyscaleToolStripMenuItem.Text = "Greyscale";
+            this.greyscaleToolStripMenuItem.Click += new System.EventHandler(this.greyscaleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveStateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitApplicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadStateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greyscaleToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentCode
+{
+    public partial class Form1 : Form
+    {
+        private const int MAX = 256;      // max iterations
+        private const double SX = -2.025; // start value real
+        private const double SY = -1.125; // start value imaginary
+        private const double EX = 0.6;    // end value real
+        private const double EY = 1.125;  // end value imaginary
+        private static int x1 = 640, y1 = 480, xs, ys, xe, ye;
+        private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static float xy;
+        Rectangle rec;
+        public System.Drawing.Bitmap bitmap;
+        //private Image picture;
+        private Graphics g1;
+
+
+
+
+        private HSB HSBcol = new HSB();
+        // Saves image to C Drive when save button is pressed
+        private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                bitmap.Save(sfd.FileName, format);
+            }
+        }
+        // Saves state to rows.txt file when save state button is pressed
+        private void saveStateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string savexzoom = xzoom.ToString();
+            string saveyzoom = yzoom.ToString();
+            string saveystart = ystart.ToString();
+            string savexstart = xstart.ToString();
+            string[] rows = { savexzoom, saveyzoom, savexstart, saveystart };
+
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.FilterIndex = 2;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.File.WriteAllLines(sfd.FileName, rows);
+                }
+            }
+        }
+        // Uses exit function when exit button is pressed
+        private void exitApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        // Reads state from rows.txt then loadsthe state after load state button is pressed
+        private void loadStateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            string line = "";
+            var rows = new List<string>();
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                xzoom = System.Convert.ToDouble(rows[0]);
+                yzoom = System.Convert.ToDouble(rows[1]);
+                xstart = System.Convert.ToDouble(rows[2]);
+                ystart = System.Convert.ToDouble(rows[3]);
+
+            }
+            
+
+
+           
+
+            mandelbrot();
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Instantiates form and initialises values
+        public Form1()
+        {
+            this.DoubleBuffered = true;
+            init();
+            start();
+            InitializeComponent();
+        }
+
+
+
+
+        public void init() // all instances will be prepared
+        {
+            //HSBcol = new HSB();
+            this.Width = 640;
+            this.Height = 480;
+
+            //finished = false;
+
+            //c1 = Cursors.Default;
+            //c2 = Cursors.IBeam;
+            xy = (float)x1 / (float)y1;
+            //picture = createImage(x1, y1);
+            //g1 = picture.getGraphics();
+            bitmap = new Bitmap(x1, y1);
+            g1 = Graphics.FromImage(bitmap);
+            // finished = true;
+        }
+
+
+
+        public void start()
+        {
+            //action = false;
+            //rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            mandelbrot();
+        }
+
+
+
+
+        private void mandelbrot() // calculate all points
+        {
+            int x, y;
+            float h, b, alt = 0.0f;
+            Color color;
+            Pen fractPen = new Pen(Color.Black);
+            // action = false;
+            //  setCursor(c1);
+            // showStatus("Mandelbrot-Set will be produced - please wait...");
+            for (x = 0; x < x1; x += 2)
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        b = 1.0f - h * h;
+                        // brightnes
+                        ///djm added
+                        ///HSBcol.fromHSB(h,0.8f,b); //convert hsb to rgb then make a Java Color
+                        color = HSB.FromHSB(new HSB(h * 255, 0.8f * 255, b * 255));
+                        ///g1.setColor(col);
+                        //djm end
+                        //djm added to convert to RGB from HSB
+
+                        // g1.setColor(Color.getHSBColor(h, 0.8f, b));
+                        //djm test
+                        // Color col = Color.getHSBColor(h, 0.8f, b);
+                        // int red = col.getRed();
+                        // int green = col.getGreen();
+                        // int blue = col.getBlue();
+                        //djm 
+                        fractPen = new Pen(color);
+                        alt = h;
+                    }
+
+                    g1.DrawLine(fractPen, x, y, x + 1, y);
+                }
+            //showStatus("Mandelbrot-Set ready - please select zoom area with pressed mouse.");
+            //setCursor(c2);
+            //action = true;
+        }
+
+        private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
+        {
+            double r = 0.0, i = 0.0, m = 0.0;
+            int j = 0;
+
+            while ((j < MAX) && (m < 4.0))
+            {
+                j++;
+                m = r * r - i * i;
+                i = 2.0 * r * i + ywert;
+                r = m + xwert;
+            }
+            return (float)j / (float)MAX;
+        }
+
+        private void initvalues() // reset start values
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+                xstart = xende - (yende - ystart) * (double)xy;
+        }
+
+        //  public void mousePressed(MouseEvent e)
+        //{
+
+        //}
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g1 = e.Graphics;
+            g1.DrawImage(bitmap, 0, 0, x1, y1);
+            using (Pen pen = new Pen(Color.White, 1))
+            {
+                e.Graphics.DrawRectangle(pen, rec);
+
+            }
+            Invalidate();
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int z, w;
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs > xe)
+                {
+                    z = xs;
+                    xs = xe;
+                    xe = z;
+                }
+                if (ys > ye)
+                {
+                    z = ys;
+                    ys = ye;
+                    ye = z;
+                }
+                w = (xe - xs);
+                z = (ye - ys);
+                if ((w < 2) && (z < 2)) initvalues();
+                else
+                {
+                    if (((float)w > (float)z * xy)) ye = (int)((float)ys + (float)w / xy);
+                    else xe = (int)((float)xs + (float)z * xy);
+                    xende = xstart + xzoom * (double)xe;
+                    yende = ystart + yzoom * (double)ye;
+                    xstart += xzoom * (double)xs;
+                    ystart += yzoom * (double)ys;
+                }
+                xzoom = (xende - xstart) / (double)x1;
+                yzoom = (yende - ystart) / (double)y1;
+                mandelbrot();
+                this.Invalidate();
+            }
+        }
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+
+                xs = e.X;
+                ys = e.Y;
+                this.Invalidate();
+            }
+        }
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs < xe)
+                {
+                    if (ys < ye) rec = new Rectangle(xs, ys, (xe - xs), (ye - ys));
+                    else rec = new Rectangle(xs, ye, (xe - xs), (ys - ye));
+                }
+                else
+                {
+                    if (ys < ye) rec = new Rectangle(xe, ys, (xs - xe), (ye - ys));
+                    else rec = new Rectangle(xe, ye, (xs - xe), (ys - ye));
+                }
+
+
+                this.Invalidate();
+            }
+
+
+        }
+
+
+        //Don't think this is needed due to APPLET
+        // public String getAppletInfo()
+        //{
+        //   return "fractal.class - Mandelbrot Set a Java Applet by Eckhard Roessel 2000-2001";
+        //}
+    }
+
+    
+        public struct HSB
+        {
+            float h;
+            float s;
+            float b;
+            int a;
+            public HSB(float h, float s, float b)
+            {
+                this.a = 0xff;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+
+            public HSB(int a, float h, float s, float b)
+            {
+                this.a = a;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+            public float H { get { return h; } }
+            public float S { get { return s; } }
+            public float B { get { return b; } }
+            public int A { get { return a; } }
+            public Color Color { get { return FromHSB(this); } }
+            public static Color FromHSB(HSB hsbColor)
+            {
+                float red = hsbColor.b;
+                float green = hsbColor.b;
+                float blue = hsbColor.b;
+                if (hsbColor.s != 0)
+                {
+                    float max = hsbColor.b;
+                    float dif = hsbColor.b * hsbColor.s / 255f;
+                    float min = hsbColor.b - dif;
+                    float h2 = hsbColor.h * 360f / 255f;
+
+                    if (h2 < 60f)
+                    {
+                        red = max;
+                        green = h2 * dif / 60f + min;
+                        blue = min;
+                    }
+                    else if (h2 < 120f)
+                    {
+                        red = -(h2 - 120f) * dif / 60f + min;
+                        green = max;
+                        blue = min;
+                    }
+                    else if (h2 < 180f)
+                    {
+                        red = min;
+                        green = max;
+                        blue = (h2 - 120f) * dif / 60f + min;
+                    }
+                    else if (h2 < 240f)
+                    {
+                        red = min;
+                        green = -(h2 - 240f) * dif / 60f + min;
+                        blue = max;
+                    }
+                    else if (h2 < 300f)
+                    {
+                        red = (h2 - 240f) * dif / 60f + min;
+                        green = min;
+                        blue = max;
+                    }
+                    else if (h2 <= 360f)
+                    {
+                        red = max;
+                        green = min;
+                        blue = -(h2 - 360f) * dif / 60 + min;
+                    }
+                    else
+                    {
+                        red = 0;
+                        green = 0;
+                        blue = 0;
+                    }
+                }
+                return Color.FromArgb(hsbColor.a, (int)Math.Round(Math.Min(Math.Max(red, 0), 255)), (int)Math.Round(Math.Min(Math.Max(green, 0), 255)), (int)Math.Round(Math.Min(Math.Max(blue, 0), 255)));
+            }
+
+        }
+    }
+
